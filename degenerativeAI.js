82cf2632d7db50cba77a18ce908b3175e162ee6a
AndrianMiora3 <mioraandrian23@gmail.com>
2025-06-19 19:55:45 +0200

    feat: start of degenerativeAI.js, and adding a new methodsTry.js to store Methods I try

--- degenerativeAI.js
+++ degenerativeAI.js
@@ -1,3 +1,9 @@
-export const degenerativeAI = ([N, L, C]) => {
-    return;
+export const degenerativeAI = (spool = [N, {L: C}]) => {
+    let finalLength;
+    const [N, L] = spool; 
+    console.log(N);
+    console.log(Object.entries(L));
+    
 }
+
+degenerativeAI([7, {11: 'yellow', 8: 'blue', 3: 'red', 2: 'green', 4: 'blue', 5: 'red', 2: 'red'}]);
---
+++ methodsTry.js
@@ -0,0 +1,5 @@
+let object = {1: 'something', 2: 'another thing'};
+
+let arrayObject = Array.from(object);
+
+console.log(arrayObject);
